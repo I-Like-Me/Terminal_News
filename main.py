f022dcb22b1ba57401cfbde38401514ff4596a0e
cassgolkin@gmail.com <cassgolkin@gmail.com>
2022-12-26 18:04:55 -0500

    Accomplished nothing LOL

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
-def hello():
-    return 'Hello, World!'
+def home():
+    return '''<h1>Hello, World!</h1>
+    <=input name="text">
+    <a href="/articles/<entry>">Return to home page</a>
+    '''
+    #<input type="submit">
+
+@app.route('/articles/<article_name>')
+def article(article_name):
+    return f'''
+    <h2>{article_name.replace('-', ' ').title()}</h2>
+    <a href="/">Return to home page</a>
+    '''
